1a51549fbd03a65ac94b781b54eb3324805c7be5
Tomasz Cekało <tomasz.cekalo@gmail.com>
2022-01-14 09:01:40 +0100

    Add project files.

---
+++ SalesManago.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace SalesManago.Microsoft.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSalesManago(this IServiceCollection services,
+            IConfigurationSection configurationSection)
+        {
+            services.AddHttpClient<ISalesManagoService, SalesManagoService>();
+            var config = configurationSection.Get<SalesManagoSettings>();
+            services.AddSingleton<SalesManagoSettings>(config);
+
+            return services;
+        }
+    }
+}
